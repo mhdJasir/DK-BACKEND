d902500462f18b26a7c84852a17ed4c76252c590
jasir <muhammedjasirch@gmail.com>
2023-08-23 23:04:27 +0530

    db auth

--- db/dbConnection.js
+++ db/dbConnection.js
@@ -5,16 +5,19 @@ require("dotenv").config()
 const connectionParams = {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    authSource: "admin",
+    user: process.env.USER_NAME,
+    pass: process.env.PASSWORD
 };
 
 
 const connectDB =async ()=>{
-    mongoose.set({ strictQuery: false })
-    await mongoose.connect(process.env.MONGO_URL, connectionParams).then(() => {
+    try {
+        await mongoose.connect(process.env.MONGO_URL, connectionParams);
         console.log("Connected to DB");
-    }).catch(() => {
-        console.log("Connection to DB failed");
-    })
+    } catch (error) {
+        console.error("Connection to DB failed:", error);
+    }
 }
 
 module.exports= connectDB;
